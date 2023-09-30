ffa234714d17c68393d120c4704a7fda4e7b6a84
Kanha_Github <kanhakhantaal@gmail.com>
2023-09-30 14:22:18 +0530

    UI and command added

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
+import tkinter
+import customtkinter
+
+def urlSearch():
+    print("checked")
+
+
+
+customtkinter.set_appearance_mode("System")
+
+app=customtkinter.CTk()
+app.geometry("720x480")
+app.title("NDM")
+
+# Input box
+urlLabel=customtkinter.CTkLabel(app, text="Add you link",command=urlSearch)
+urlLabel_corner_radius(5)
+urlLabel.pack()
+
+
+button=tkinter.Button(app, text="Search")
+button.grid(row=1,column=2)
+button.pack()
+
+
+
 if __name__=='__main__':
     pass
+
+
+
+app.mainloop()
